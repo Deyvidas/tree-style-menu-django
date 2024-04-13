ea4b4fc197af3b2a5de9c65c285835260d3b4604
Deyvidas <devidbanyavichyus@yandex.ru>
2024-04-13 11:47:31 +0400

    Make an auto-refresh of the cached QuerySet when it is changed (on delete, update, or create).

--- backend/menu/models.py
+++ backend/menu/models.py
@@ -1,17 +1,64 @@
 from __future__ import annotations
 
 from typing import Self
+from typing import TypedDict
 
+from django.core.cache import cache
 from django.db import models
+from django.db.models.signals import post_delete
+from django.db.models.signals import post_save
 from django.db.models.signals import pre_save
+from django.forms import model_to_dict
 from slugify import slugify
 
 
 NAME_MAX_LENGTH = 30
 HREF_MAX_LENGTH = 30
 
+CACHE_TIMEOUT_SEC = 60 * 5
+MENU_QS_CACHE_KEY = 'menu_qs'
+
+
+type TQS = models.QuerySet['Menu']
+
+
+class TMenuItem(TypedDict):
+    id: int
+    name: str
+    href: str
+    parent: int | None
+    childs: list[TMenuItem]
+
+
+class MenuManger(models.Manager):
+    def get_queryset(self) -> TQS:
+        return self.get_cached_menu()
+
+    def with_childrens(self, parent_id) -> TMenuItem:
+        menu_set = [TMenuItem(**model_to_dict(i)) for i in self.all()]
+        return self._get_tree(parent_id, menu_set)
+
+    def _get_tree(self, parent_id: int, menu: list[TMenuItem]) -> TMenuItem:
+        parent = list(filter(lambda i: i['id'] == parent_id, menu))[0]
+        childs = list(filter(lambda i: i['parent'] == parent_id, menu))
+        parent['childs'] = [self._get_tree(c['id'], menu) for c in childs]
+        return parent
+
+    def get_cached_menu(self) -> TQS:
+        query_set: TQS | None = cache.get(MENU_QS_CACHE_KEY)
+        if query_set is None:
+            query_set = self.actualize_cached_menu()
+        return query_set
+
+    def actualize_cached_menu(self) -> TQS:
+        new_query_set = super().get_queryset()
+        cache.set(MENU_QS_CACHE_KEY, new_query_set, CACHE_TIMEOUT_SEC)
+        return new_query_set
+
 
 class Menu(models.Model):
+    objects: MenuManger = MenuManger()
+
     name = models.CharField(
         verbose_name='link name',
         max_length=NAME_MAX_LENGTH,
@@ -31,14 +78,6 @@ class Menu(models.Model):
 
     childrens: models.Manager[Self]
 
-    @property
-    def with_childrens(self):
-        return {
-            'name': self.name,
-            'href': self.href,
-            'childrens': [c.with_childrens for c in self.childrens.all()],
-        }
-
     class Meta:
         verbose_name = 'Меню'
         verbose_name_plural = 'Меню'
@@ -47,9 +86,8 @@ class Menu(models.Model):
         return self.__repr__()
 
     def __repr__(self) -> str:
-        fields = ('name', 'href')
-        fields = ', '.join([f'{f}: {repr(getattr(self, f))}' for f in fields])
-        return f'{type(self).__name__} - ' + '{' + fields + '}'
+        fields = ('id', 'name', 'href')
+        return f'{self.__class__.__name__}({model_to_dict(self, fields)})'
 
 
 def menu_pre_save(instance: Menu, *args, **kwargs):
@@ -58,3 +96,17 @@ def menu_pre_save(instance: Menu, *args, **kwargs):
 
 
 pre_save.connect(receiver=menu_pre_save, sender=Menu)
+
+
+def menu_post_save(sender: Menu, *args, **kwargs):
+    sender.objects.actualize_cached_menu()
+
+
+post_save.connect(receiver=menu_post_save, sender=Menu)
+
+
+def menu_post_delete(instance: Menu, *args, **kwargs):
+    instance._meta.model.objects.actualize_cached_menu()
+
+
+post_delete.connect(receiver=menu_post_delete, sender=Menu)
--- backend/menu/views.py
+++ backend/menu/views.py
@@ -1,35 +1,12 @@
 from __future__ import annotations
 
 import json
-from typing import TypedDict
 
 from django import http
-from django.core.cache import cache
 from django.shortcuts import render
 from menu.models import Menu
 
 
 def menu(request: http.HttpRequest) -> http.HttpResponse:
-    query_set = cache.get('query_set')
-    if query_set is None:
-        query_set = list(map(lambda i: TItem(**i), Menu.objects.values()))
-        cache.set('query_set', query_set)
-
-    res = get_childrens(19, query_set)
-    return render(request, 'menu.html', {'menu_items': json.dumps([res])})
-
-
-class TItem(TypedDict):
-    id: int
-    name: str
-    href: str
-    parent_id: int | None
-    childs: list[TItem]
-
-
-def get_childrens(parent_id: int | None, query_set: list[TItem]) -> TItem:
-    parent = list(filter(lambda i: i['id'] == parent_id, query_set))[0]
-    childs = list(filter(lambda i: i['parent_id'] == parent_id, query_set))
-    parent['childs'] = [get_childrens(c['id'], query_set) for c in childs]
-
-    return parent
+    context = {'menu_items': json.dumps([Menu.objects.with_childrens(19)])}
+    return render(request, 'menu.html', context)
